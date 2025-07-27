a9b46feb5431f7ab9f01afd932c02bdf95a8ab3b
d940909492 <dong5455993@gmail.com>
2025-07-27 00:45:28 -0400

    update

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
-import socket  # noqa: F401
+import socket
 
 
 def main():
     print("Logs from your program will appear here!")
-    
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    #server_socket.accept() # wait for client
-    connection, address = server_socket.accept() # wait for client
 
-    connection.sendall(b"+PONG\r\n")
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    client_socket, _ = server_socket.accept()  # wait for client
+
+    while True:
+        request: bytes = client_socket.recv(512)
+        data: str = request.decode()
+
+        if "ping" in data.lower():
+            client_socket.send("+PONG\r\n".encode())
 
 
 if __name__ == "__main__":
---
+++ red.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Use this script to run your program LOCALLY.
+#
+# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
+#
+# Learn more: https://codecrafters.io/program-interface
+
+set -e # Exit early if any commands fail
+
+# Run the Python application directly
+python -m app.main "$@"
