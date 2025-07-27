d75a734c3342e282e936668462e22c8a4dd65ca7
d940909492 <dong5455993@gmail.com>
2025-07-26 20:41:41 -0400

    update

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ def main():
     print("Logs from your program will appear here!")
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    #server_socket.accept() # wait for client
+    connection, address = server_socket.accept() # wait for client
+
+    connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
