0c06897d5eb420b83d7148c2cfb81187f57cd8da
d940909492 <dong5455993@gmail.com>
2025-07-28 21:26:08 -0400

    update and fix

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 
+DATA_STORE = {}
 
 def handle_client(client_socket, client_address):
     print(f"Connect from {client_address}")
@@ -10,17 +11,34 @@ def handle_client(client_socket, client_address):
             if not request_bytes:
                 break
 
+            # resp parsing
             parts = request_bytes.strip().split(b'\r\n')
-            
             command = parts[2].decode().upper()
 
+            # command handling
             if command == "PING":
-                response = b"+PONG\r\n" #
+                response = b"+PONG\r\n"
                 client_socket.sendall(response)
+
             elif command == "ECHO":
                 message = parts[4]
                 response = f"${len(message)}\r\n".encode() + message + b"\r\n"
                 client_socket.sendall(response)
+
+            elif command == "SET":
+                key = parts[4]
+                value = parts[6]
+                DATA_STORE[key] = value
+                client_socket.sendall(b"+OK\r\n")
+
+            elif command == "GET":
+                key = parts[4]
+                value = DATA_STORE.get(key)
+                if value:
+                    response = f"${len(value)}\r\n".encode() + value + b"\r\n"
+                    client_socket.sendall(response)
+                else:
+                    client_socket.sendall(b"$-1\r\n")
             else:
                 client_socket.sendall(b"-ERR unknown command\r\n")
 
@@ -32,16 +50,15 @@ def handle_client(client_socket, client_address):
 
 
 def main():
-    print("Redis server start...") #
+    print("Redis server start...")
     
     server_socket = socket.create_server(("localhost", 6379))
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print("Server listen on localhost 6379") #
+    print("Server listen on localhost 6379")
     
     while True:
         client_socket, client_address = server_socket.accept()
         
-        # Start a new thread to for client
         client_thread = threading.Thread(
             target=handle_client,
             args=(client_socket, client_address),
