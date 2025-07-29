9f97fbce762d7fc59a7fef29ee976476ba9b4710
d940909492 <dong5455993@gmail.com>
2025-07-28 21:09:43 -0400

    update and fix

--- app/main.py
+++ app/main.py
@@ -4,34 +4,49 @@ import threading
 
 def handle_client(client_socket, client_address):
     print(f"Connect from {client_address}")
-    request = client_socket.recv(512) 
-    
-    if request:
-        data = request.decode().strip().split('\r\n')
-        
-        if len(data) >= 5 and data[0] == '*2' and data[2].lower() == '$4' and data[3].lower() == 'echo':
-            arg_length = int(data[4][1:])
-            arg = data[5]
-            response = f"${arg_length}\r\n{arg}\r\n"
-        else:
-            response = "+PONG\r\n"
+    while True:
+        try:
+            request_bytes = client_socket.recv(1024)
+            if not request_bytes:
+                break
+
+            parts = request_bytes.strip().split(b'\r\n')
             
-        client_socket.send(response.encode())
+            command = parts[2].decode().upper()
+
+            if command == "PING":
+                response = b"+PONG\r\n" #
+                client_socket.sendall(response)
+            elif command == "ECHO":
+                message = parts[4]
+                response = f"${len(message)}\r\n".encode() + message + b"\r\n"
+                client_socket.sendall(response)
+            else:
+                client_socket.sendall(b"-ERR unknown command\r\n")
+
+        except (IndexError, ConnectionResetError):
+            break
+    
+    print(f"Closing connection from {client_address}")
+    client_socket.close()
 
 
 def main():
-    print("Redis server start...")
-
+    print("Redis server start...") #
+    
     server_socket = socket.create_server(("localhost", 6379))
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print("Server listen on localhost 6379")
+    print("Server listen on localhost 6379") #
     
     while True:
         client_socket, client_address = server_socket.accept()
-
-        # Start a new thread for each client
-        client_thread = threading.Thread(target=handle_client,
-        args=(client_socket, client_address),daemon=True)
+        
+        # Start a new thread to for client
+        client_thread = threading.Thread(
+            target=handle_client,
+            args=(client_socket, client_address),
+            daemon=True
+        )
         client_thread.start()
 
 
