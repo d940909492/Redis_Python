ad11656741dda5b245aa6cddc4d0c5836c7fa3a5
d940909492 <dong5455993@gmail.com>
2025-08-06 13:42:49 -0400

    update

--- app/main.py
+++ app/main.py
@@ -40,186 +40,188 @@ def handle_client(client_socket, client_address):
             elif command == "XADD":
                 key = parts[4]
                 entry_id_str = parts[6].decode()
-                
-                if entry_id_str == '*':
-                    ms_time = 0
-                    seq_num = 0
-                    with GLOBAL_LOCK:
+                response = b""
+
+                with GLOBAL_LOCK:
+                    if entry_id_str == '*':
                         ms_time = int(time.time() * 1000)
-                        
+                        seq_num = 0
                         stored_item = DATA_STORE.get(key)
                         if stored_item and stored_item[0] == 'stream' and stored_item[1]:
-                            last_entry_id_str = stored_item[1][-1][0].decode()
-                            last_ms_time, last_seq_num = map(int, last_entry_id_str.split('-'))
-                            
-                            if ms_time <= last_ms_time:
-                                ms_time = last_ms_time
-                                seq_num = last_seq_num + 1
-                        
+                            last_ms, last_seq = map(int, stored_item[1][-1][0].decode().split('-'))
+                            if ms_time <= last_ms:
+                                ms_time = last_ms
+                                seq_num = last_seq + 1
                         generated_id_str = f"{ms_time}-{seq_num}"
                         generated_id_bytes = generated_id_str.encode()
-
-                        entry_data = {}
-                        field_value_parts = parts[8::2]
-                        for i in range(0, len(field_value_parts), 2):
-                            entry_data[field_value_parts[i]] = field_value_parts[i+1]
-                        
-                        new_entry = (generated_id_bytes, entry_data)
-                        
-                        if stored_item and stored_item[0] == 'stream':
-                            stored_item[1].append(new_entry)
-                        else:
-                            DATA_STORE[key] = ('stream', [new_entry])
-
-                    response = f"${len(generated_id_bytes)}\r\n".encode() + generated_id_bytes + b"\r\n"
-                    client_socket.sendall(response)
-                
-                elif entry_id_str.endswith('-*'):
-                    ms_time_str = entry_id_str[:-2]
-                    try:
-                        ms_time = int(ms_time_str)
-                    except ValueError:
-                        client_socket.sendall(b"-ERR Invalid stream ID specified\r\n")
-                        continue
-                    
-                    seq_num = 0
-                    with GLOBAL_LOCK:
+                        response = f"${len(generated_id_bytes)}\r\n".encode() + generated_id_bytes + b"\r\n"
+                        entry_id_bytes_to_store = generated_id_bytes
+                    elif entry_id_str.endswith('-*'):
+                        ms_time = int(entry_id_str.split('-')[0])
+                        seq_num = 0
                         stored_item = DATA_STORE.get(key)
                         if stored_item and stored_item[0] == 'stream' and stored_item[1]:
-                            stream_entries = stored_item[1]
-                            last_entry_id_str = stream_entries[-1][0].decode()
-                            last_ms_time, last_seq_num = map(int, last_entry_id_str.split('-'))
-
-                            if ms_time < last_ms_time:
+                            last_ms, last_seq = map(int, stored_item[1][-1][0].decode().split('-'))
+                            if ms_time < last_ms:
                                 client_socket.sendall(b"-ERR The ID specified in XADD is equal or smaller than the target stream top item\r\n")
                                 continue
-                            
-                            if ms_time == last_ms_time:
-                                seq_num = last_seq_num + 1
-                        
+                            if ms_time == last_ms:
+                                seq_num = last_seq + 1
                         if ms_time == 0 and seq_num == 0:
                             seq_num = 1
-
                         generated_id_str = f"{ms_time}-{seq_num}"
                         generated_id_bytes = generated_id_str.encode()
-
-                        entry_data = {}
-                        field_value_parts = parts[8::2]
-                        for i in range(0, len(field_value_parts), 2):
-                            entry_data[field_value_parts[i]] = field_value_parts[i+1]
-                        
-                        new_entry = (generated_id_bytes, entry_data)
-                        
-                        if stored_item and stored_item[0] == 'stream':
-                            stored_item[1].append(new_entry)
-                        else:
-                            DATA_STORE[key] = ('stream', [new_entry])
-
-                    response = f"${len(generated_id_bytes)}\r\n".encode() + generated_id_bytes + b"\r\n"
-                    client_socket.sendall(response)
-                
-                else:
-                    try:
+                        response = f"${len(generated_id_bytes)}\r\n".encode() + generated_id_bytes + b"\r\n"
+                        entry_id_bytes_to_store = generated_id_bytes
+                    else:
                         ms_time, seq_num = map(int, entry_id_str.split('-'))
-                    except ValueError:
-                        client_socket.sendall(b"-ERR Invalid stream ID specified\r\n")
-                        continue
-
-                    if ms_time == 0 and seq_num == 0:
-                        client_socket.sendall(b"-ERR The ID specified in XADD must be greater than 0-0\r\n")
-                        continue
-
-                    with GLOBAL_LOCK:
+                        if ms_time == 0 and seq_num == 0:
+                            client_socket.sendall(b"-ERR The ID specified in XADD must be greater than 0-0\r\n")
+                            continue
                         stored_item = DATA_STORE.get(key)
-                        
                         if stored_item and stored_item[0] == 'stream' and stored_item[1]:
-                            stream_entries = stored_item[1]
-                            last_entry_id_str = stream_entries[-1][0].decode()
-                            last_ms_time, last_seq_num = map(int, last_entry_id_str.split('-'))
-                            
-                            if ms_time < last_ms_time or (ms_time == last_ms_time and seq_num <= last_seq_num):
+                            last_ms, last_seq = map(int, stored_item[1][-1][0].decode().split('-'))
+                            if ms_time < last_ms or (ms_time == last_ms and seq_num <= last_seq):
                                 client_socket.sendall(b"-ERR The ID specified in XADD is equal or smaller than the target stream top item\r\n")
                                 continue
-                        
-                        entry_data = {}
-                        field_value_parts = parts[8::2]
-                        for i in range(0, len(field_value_parts), 2):
-                            field = field_value_parts[i]
-                            value = field_value_parts[i+1]
-                            entry_data[field] = value
-                        
-                        new_entry = (parts[6], entry_data)
-                        
-                        if stored_item and stored_item[0] == 'stream':
-                            stream_entries.append(new_entry)
-                        else:
-                            DATA_STORE[key] = ('stream', [new_entry])
+                        response = f"${len(parts[6])}\r\n".encode() + parts[6] + b"\r\n"
+                        entry_id_bytes_to_store = parts[6]
 
-                    response = f"${len(parts[6])}\r\n".encode() + parts[6] + b"\r\n"
-                    client_socket.sendall(response)
-            
+                    entry_data = {parts[i]: parts[i+2] for i in range(7, len(parts)-1, 4)}
+                    new_entry = (entry_id_bytes_to_store, entry_data)
+                    
+                    stored_item = DATA_STORE.get(key)
+                    if stored_item and stored_item[0] == 'stream':
+                        stored_item[1].append(new_entry)
+                    else:
+                        DATA_STORE[key] = ('stream', [new_entry])
+                    
+                    if key in BLOCKING_CONDITIONS:
+                        BLOCKING_CONDITIONS[key].notify_all()
+                
+                client_socket.sendall(response)
+
             elif command == "XREAD":
+                is_blocking = False
+                timeout_ms = 0
+                
                 try:
+                    block_keyword_idx = -1
+                    for i, p in enumerate(parts):
+                        if p.lower() == b'block':
+                            block_keyword_idx = i
+                            break
+                    
+                    if block_keyword_idx != -1:
+                        is_blocking = True
+                        timeout_ms = int(parts[block_keyword_idx + 2].decode())
+                    
                     streams_keyword_idx = -1
-                    for i, part in enumerate(parts):
-                        if part.lower() == b'streams':
+                    for i, p in enumerate(parts):
+                        if p.lower() == b'streams':
                             streams_keyword_idx = i
                             break
-                    if streams_keyword_idx == -1:
-                        raise ValueError("streams keyword not found")
 
-                    num_args_after_streams = len(parts) - streams_keyword_idx - 1
-                    num_streams = num_args_after_streams // 4
-
+                    num_keys = (len(parts) - streams_keyword_idx - 1) // 4
                     keys_start_idx = streams_keyword_idx + 2
-                    ids_start_idx = keys_start_idx + (num_streams * 2)
-
-                    keys = parts[keys_start_idx : ids_start_idx : 2]
+                    ids_start_idx = keys_start_idx + (num_keys * 2)
+                    keys = parts[keys_start_idx:ids_start_idx:2]
                     start_ids_str = [p.decode() for p in parts[ids_start_idx::2]]
-
-                    if len(keys) != len(start_ids_str):
-                        raise ValueError("Mismatch between keys and IDs")
+                    
                 except (ValueError, IndexError):
                     client_socket.sendall(b"-ERR syntax error\r\n")
                     continue
-                
-                def parse_complete_id(id_str):
+
+                def parse_id(id_str):
+                    if id_str == '$':
+                        return '$'
                     ms, seq = map(int, id_str.split('-'))
                     return (ms, seq)
 
+                def find_entries(key, start_id):
+                    results = []
+                    _stored_item = DATA_STORE.get(key)
+                    if _stored_item and _stored_item[0] == 'stream':
+                        for entry_id_bytes, entry_data in _stored_item[1]:
+                            entry_id_tuple = parse_id(entry_id_bytes.decode())
+                            if entry_id_tuple > start_id:
+                                results.append((entry_id_bytes, entry_data))
+                    return results
+
                 all_results = {}
                 with GLOBAL_LOCK:
-                    for i in range(num_streams):
+                    for i in range(num_keys):
                         key = keys[i]
-                        start_id = parse_complete_id(start_ids_str[i])
+                        start_id_val = start_ids_str[i]
                         
-                        key_results = []
-                        stored_item = DATA_STORE.get(key)
-                        if stored_item and stored_item[0] == 'stream':
-                            for entry_id_bytes, entry_data in stored_item[1]:
-                                entry_id_tuple = parse_complete_id(entry_id_bytes.decode())
-                                if entry_id_tuple > start_id:
-                                    key_results.append((entry_id_bytes, entry_data))
+                        if start_id_val == '$':
+                            stored_item = DATA_STORE.get(key)
+                            if stored_item and stored_item[0] == 'stream' and stored_item[1]:
+                                start_id = parse_id(stored_item[1][-1][0].decode())
+                            else:
+                                start_id = (0, 0)
+                        else:
+                            start_id = parse_id(start_id_val)
                         
+                        key_results = find_entries(key, start_id)
                         if key_results:
                             all_results[key] = key_results
                 
-                if not all_results:
-                    client_socket.sendall(b"$-1\r\n")
+                if all_results or not is_blocking:
+                    if not all_results:
+                        client_socket.sendall(b"$-1\r\n")
+                    else:
+                        response_parts = [f"*{len(all_results)}\r\n".encode()]
+                        for key, results in all_results.items():
+                            response_parts.append(b"*2\r\n")
+                            response_parts.append(f"${len(key)}\r\n".encode() + key + b"\r\n")
+                            response_parts.append(f"*{len(results)}\r\n".encode())
+                            for entry_id_bytes, entry_data in results:
+                                response_parts.append(b'*2\r\n')
+                                response_parts.append(f"${len(entry_id_bytes)}\r\n".encode() + entry_id_bytes + b"\r\n")
+                                flat_kv = [item for pair in entry_data.items() for item in pair]
+                                response_parts.append(f"*{len(flat_kv)}\r\n".encode())
+                                for item in flat_kv:
+                                    response_parts.append(f"${len(item)}\r\n".encode() + item + b"\r\n")
+                        client_socket.sendall(b"".join(response_parts))
                 else:
-                    response_parts = [f"*{len(all_results)}\r\n".encode()]
-                    for key, results in all_results.items():
-                        response_parts.append(b"*2\r\n")
-                        response_parts.append(f"${len(key)}\r\n".encode() + key + b"\r\n")
-                        response_parts.append(f"*{len(results)}\r\n".encode())
-                        for entry_id_bytes, entry_data in results:
-                            response_parts.append(b'*2\r\n')
-                            response_parts.append(f"${len(entry_id_bytes)}\r\n".encode() + entry_id_bytes + b"\r\n")
-                            flat_kv = [item for pair in entry_data.items() for item in pair]
-                            response_parts.append(f"*{len(flat_kv)}\r\n".encode())
-                            for item in flat_kv:
-                                response_parts.append(f"${len(item)}\r\n".encode() + item + b"\r\n")
-                    client_socket.sendall(b"".join(response_parts))
+                    with GLOBAL_LOCK:
+                        block_key = keys[0]
+                        if block_key not in BLOCKING_CONDITIONS:
+                            BLOCKING_CONDITIONS[block_key] = threading.Condition(GLOBAL_LOCK)
+                        condition = BLOCKING_CONDITIONS[block_key]
+                        
+                        timeout_sec = timeout_ms / 1000.0
+                        was_notified = condition.wait(timeout=timeout_sec)
+                        
+                        if was_notified:
+                            for i in range(num_keys):
+                                key = keys[i]
+                                start_id_val = start_ids_str[i]
+                                start_id = parse_id(start_id_val)
+                                key_results = find_entries(key, start_id)
+                                if key_results:
+                                    all_results[key] = key_results
+                        
+                        if not condition._waiters:
+                            del BLOCKING_CONDITIONS[block_key]
+                    
+                    if not all_results:
+                        client_socket.sendall(b"$-1\r\n")
+                    else:
+                        response_parts = [f"*{len(all_results)}\r\n".encode()]
+                        for key, results in all_results.items():
+                            response_parts.append(b"*2\r\n")
+                            response_parts.append(f"${len(key)}\r\n".encode() + key + b"\r\n")
+                            response_parts.append(f"*{len(results)}\r\n".encode())
+                            for entry_id_bytes, entry_data in results:
+                                response_parts.append(b'*2\r\n')
+                                response_parts.append(f"${len(entry_id_bytes)}\r\n".encode() + entry_id_bytes + b"\r\n")
+                                flat_kv = [item for pair in entry_data.items() for item in pair]
+                                response_parts.append(f"*{len(flat_kv)}\r\n".encode())
+                                for item in flat_kv:
+                                    response_parts.append(f"${len(item)}\r\n".encode() + item + b"\r\n")
+                        client_socket.sendall(b"".join(response_parts))
 
             elif command == "XRANGE":
                 key = parts[4]
